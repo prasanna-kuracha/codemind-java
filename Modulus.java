0063cb24887d69d76c7485cc2711f6c1d1e9b06a
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2024-01-05 13:06:14 +0530

    import java.util.*; public class MyClass {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int s = sc.nextInt();         int p = sc.nextInt();         System.out.println(s%p);     } }

---
+++ Modulus.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class MyClass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int p = sc.nextInt();
+        System.out.println(s%p);
+    }
+}
