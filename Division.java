a9e42316ee1341d40d3a81db9974a54fe1aa34dc
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2024-01-05 13:16:08 +0530

    import java.util.Scanner; public class MyClass{     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int p = sc.nextInt();         int q = sc.nextInt();         System.out.print(p/q);     } }

---
+++ Division.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class MyClass{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        int q = sc.nextInt();
+        System.out.print(p/q);
+    }
+}
