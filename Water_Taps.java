5aa15294b7b9064fd160bcc25b7c0008d03b79d8
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2024-01-07 12:12:22 +0530

    import java.util.*; public class MyClass{     public static void main(String args[]){         Scanner cs = new Scanner(System.in);         int a = cs.nextInt();         int b = cs.nextInt();         System.out.println((a*b)/(a+b));     } }

---
+++ Water_Taps.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String args[]){
+        Scanner cs = new Scanner(System.in);
+        int a = cs.nextInt();
+        int b = cs.nextInt();
+        System.out.println((a*b)/(a+b));
+    }
+}
