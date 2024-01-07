9e2b095492452a85e4ae859005642e20fdbc8a35
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2024-01-07 11:38:36 +0530

    import java.util.*; public class MyClass{     public static void main(String args[]){         Scanner cs = new Scanner(System.in);         int a = cs.nextInt();         int b = cs.nextInt();         int c = cs.nextInt();         System.out.println(2*c*(a+b));     } }

---
+++ Frames.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String args[]){
+        Scanner cs = new Scanner(System.in);
+        int a = cs.nextInt();
+        int b = cs.nextInt();
+        int c = cs.nextInt();
+        System.out.println(2*c*(a+b));
+    }
+}
