a9c59b500bf9d2d253211beea3d6a0ff57efc61f
prasanna-kuracha <119739744+prasanna-kuracha@users.noreply.github.com>
2024-01-12 11:39:13 +0530

    import java.util.*; public class MyClass{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         if (4*a<=1000) System.out.println("YES");         else System.out.println("NO");     } }

---
+++ Tickets.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (4*a<=1000) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
